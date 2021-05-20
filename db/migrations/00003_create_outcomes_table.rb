23b139cdf2d5fa40fa86f701c7c23c801c7e330f
iriurri <urricelquiirina@gmail.com>
2021-05-20 19:38:01 -0300

    Agregadas tablas y modelos de outcomes y careers

---
+++ db/migrations/00003_create_outcomes_table.rb
@@ -0,0 +1,12 @@
+Sequel.migration do 
+	up do
+		create_table(:outcomes) do
+			primary_key :id
+			DateTime    :created_at, default: Sequel::CURRENT_TIMESTAMP
+			DateTime    :updated_at, default: Sequel::CURRENT_TIMESTAMP
+	   	end 
+	end   
+	down do 
+		drop_table(:outcomes)
+	end
+end
