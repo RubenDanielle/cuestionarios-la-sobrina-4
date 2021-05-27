becb89bf0a114ef79257cbe4f212cbdebf76c433
iriurri <urricelquiirina@gmail.com>
2021-05-26 21:17:55 -0300

    agregadas vistas para las carreras

--- app.rb
+++ app.rb
@@ -10,6 +10,33 @@ class App < Sinatra::Base
    erb :hello_template
   end
 
+  post "/careers" do
+ 
+    career = Career.new(name: params['name'])
+
+    if career.save
+      [201, {'Location' => "careers/#{career.id}"}, 'Carrera agregada']
+    else
+      [500, {}, 'Internal Server Error']
+    end
+  end 
+
+  get "/careers" do
+    @careers=Career.all
+    erb :'careers/list_career'
+  end
+
+  get "/careers/:id" do
+    career=Career.where(id: params['id']).last
+    "<h1> Resumen de la carrera #{career.name}</h1>" +
+    "<ul>" +
+    "<li> id: #{career.id}" +
+    "<li> name: #{career.name}" +
+    "<li> survey count: #{career.surveys.count}"+
+    "</ul>"
+
+  end
+
   post "/posts" do
     request.body.rewind  # in case someone already read it
     data = JSON.parse request.body.read
