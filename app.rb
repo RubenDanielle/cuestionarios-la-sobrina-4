1f89bb228c3d2496fc5ec453102667aaf2a673bc
ayelen01 <garciiaayelen@gmail.com>
2021-10-09 19:28:28 -0300

    [#179873422] Syntax Fixes

--- app.rb
+++ app.rb
@@ -89,11 +89,11 @@ class App < Sinatra::Base
     erb :'surveys/get_career_and_dates'
   end
   get '/surveys/:careerId/from/:firstDate/to/:lastDate' do
-    @career_name = Career.where(id: params['careerId']).last.name
-    @surveys = Survey.where(careerId: params['careerId'],'created_at > ? AND created_at < ?', params['firstDate'], params['lastDate'] )
-    @first_date = params['firstDate']
-    @last_date = params['lastDate']
-    erb : 'surveys/surveys_by_date_and_career'
+    @career_name = Career.where(id: params['careerId'].to_i()).last.name
+    @surveys = Survey.where(Sequel.lit('career_id = ? AND created_at > ? AND created_at < ?', params[:careerId].to_i(), Time.parse(params[:firstDate]), Time.parse(params[:lastDate])))
+    @first_date = Time.parse(params['firstDate'])
+    @last_date = Time.parse(params['lastDate'])
+    erb :'surveys/surveys_by_date_and_career'
   end
 end
 
